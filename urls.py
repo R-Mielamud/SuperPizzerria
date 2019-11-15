69bb4e00cc5c72d9671a9714b0e0ea76c6304186
R-Mielamud <55893583+R-Mielamud@users.noreply.github.com>
2019-11-15 19:48:47 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from AllOurPizzas.views import PizzaPriceUpdateView, PizzasListView
-
-urlpatterns = [
-    url(r'^all_pizzas/$', PizzasListView.as_view()),
-    path("pizza/<int:pk>/edit/", PizzaPriceUpdateView.as_view())
-]
