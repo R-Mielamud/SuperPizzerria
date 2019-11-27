34ff478daf5ee19308cf462a0d572067ca49aeda
R-Mielamud <55893583+R-Mielamud@users.noreply.github.com>
2019-11-27 23:03:23 +0200

    Delete views.py

--- Pizzerria/allPizzas/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from allPizzas.models import Pizza
-
-class AllPizzasView(ListView):
-    model = Pizza
-    context_object_name = "all_pizzas"
-    template_name = "all_pizzas_list.html"
