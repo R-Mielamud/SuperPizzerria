405f64ae6d8a1f569f83e2373f4da395b4697467
R-Mielamud <55893583+R-Mielamud@users.noreply.github.com>
2019-11-30 19:38:49 +0200

    Delete context_processors.py

--- Pizzerria/orders/context_processors.py
+++
@@ -1,8 +0,0 @@
-from orders.models import Order
-
-def get_context(response):
-    return {
-        "separator": "---------------------------------------------------",
-        "title_part_1": "Pizzerria|",
-        "page_header": "Pizzerria",
-    }
