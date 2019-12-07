085625fb7fa66c47e42492780bb87280bbd1892d
R-Mielamud <55893583+R-Mielamud@users.noreply.github.com>
2019-12-07 18:58:22 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -1,32 +1,24 @@
 from django.db.models import *
 from django.contrib.auth.models import User
+from dishes import models
 
-class Ingredient(Model):
-    name = CharField(max_length=30, default="")
-    price = DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
-class Pizza(Model):
-    name = CharField(max_length=30, default="")
-    price = DecimalField(max_digits=10, decimal_places=2)
-    description = TextField(max_length=1000, default="")
-
-    def __str__(self):
-        return self.name
+class Pizza(models.Pizza):
+    pass
 
 class Order(Model):
     client = ForeignKey(User, related_name="orders", on_delete=CASCADE)
     pizzas = ManyToManyField("Pizza", related_name="orders")
     price = DecimalField(max_digits=100, decimal_places=2, default=0)
+    address = CharField(max_length=300, default="")
+    telephone = CharField(max_length=20, default="")
 
     def __str__(self):
         return "Order from {}".format(self.client.username)
 
     def calculate_price(self):
-        self.save(calculate=False)
+        # self.save(calculate=False)
         self.price = sum([p.price for p in self.pizzas.all()])
+        self.save(calculate=False)
 
     def save(self, calculate=True):
         if self.pk is None:
