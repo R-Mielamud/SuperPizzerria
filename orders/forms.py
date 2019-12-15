6023e3a3a22d87995c7bcb32ad9a538c7ea5abf0
R-Mielamud <55893583+R-Mielamud@users.noreply.github.com>
2019-12-15 20:53:15 +0200

    Delete forms.py

--- orders/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import *
-from orders.models import Order
-
-class ModOrderForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = ["client", "pizzas", "address", "telephone"]
